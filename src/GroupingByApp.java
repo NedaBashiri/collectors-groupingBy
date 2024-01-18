bc0d84b4529027d561b6e0e52b6b10722435eb0e
Neda bashiri <neda.bashiri.dev@gmail.com>
2024-01-18 10:49:51 +0330

    Create group-employee-by-postal-code method

--- src/GroupingByApp.java
+++ src/GroupingByApp.java
@@ -1,5 +1,7 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class GroupingByApp {
     public static void main(String[] args) {
@@ -26,6 +28,15 @@ public class GroupingByApp {
                 new Employee("Ana", "45679")
         );
         return employees;
+    }
+
+    private static Map<String, List<Employee>> groupEmployeesByPostalCodePrefix(List<Employee> employees, int minGroupSize) {
+        return employees.stream()
+                .collect(Collectors.groupingBy(
+                        employee -> employee.getPostalCode().substring(0, 2)))
+                .entrySet().stream()
+                .filter(entry -> entry.getValue().size() >= minGroupSize)
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
     }
 
 }
